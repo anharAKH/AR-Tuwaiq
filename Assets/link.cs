0cc91131ddd9cf36bdadb8bbfd67f778133579d7
anharAKH <anharAKH@gmail.com>
2024-04-03 00:19:12 +0300

    1

---
+++ Assets/link.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class link : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void twitter()
+    {
+        Application.OpenURL("https://twitter.com/TuwaiqAcademy");
+    }
+    public void linkdin()
+    {
+        Application.OpenURL("https://www.linkedin.com/company/tuwaiqacademy/?originalSubdomain=sa");
+    }
+
+}
